a0870aaa161bbcf5cdf5b5ceceee06934a6b8936
정진아 <human00024@naver.com>
2020-09-17 00:47:47 +0900

    [Feat(F)] 페이지 검증

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -30,17 +30,17 @@ const router = new VueRouter({
 // 페이지 검증
 router.beforeEach((to, from, next) => {
   // // 로그인이 필요 없는 Page
-  // const publicPages = ['Login','Introduction'] // Login 안해도 됨
-  // const authPages = ['Login'] // Login 되어있으면 안됨
+  const publicPages = ['Introduction'] // Login 안해도 됨
+  const authPages = ['Introduction'] // Login 되어있으면 안됨
 
-  // const authRequired = !publicPages.includes(to.name) // Login 해야 함
-  // const unauthRequired = authPages.includes(to.name) // Login 해서는 안됨
-  // const isLoggedIn = true // Login이 되어 있는지 
+  const authRequired = !publicPages.includes(to.name) // Login 해야 함
+  const unauthRequired = authPages.includes(to.name) // Login 해서는 안됨
+  const isLoggedIn = true // Login이 되어 있는지 
 
-  // if(unauthRequired && isLoggedIn){
-  //   next('/') // 이동
-  // }
-  // authRequired && !isLoggedIn? next({name: 'Login'}) : next()
+  if(unauthRequired && isLoggedIn){
+    next('/') // 이동
+  }
+  authRequired && !isLoggedIn? next({name: 'Introduction'}) : next()
   
 })
 
