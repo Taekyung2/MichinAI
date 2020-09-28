87f31d2b176d9e4273ad4d588f8fb6d9031dbdc7
ltk3934 <ltk3934@daum.net>
2020-09-28 16:37:11 +0900

    [Feat(B)] WordBook API 추

--- backend/src/main/java/com/michin/ai/word/dto/response/WordbookResult.java
+++ backend/src/main/java/com/michin/ai/word/dto/response/WordbookResult.java
@@ -1,10 +1,12 @@
-package com.michin.ai.controller.result;
+package com.michin.ai.word.dto.response;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
+import com.michin.ai.common.ApiResult;
+import com.michin.ai.common.Result;
 import com.michin.ai.word.model.Word;
 import com.michin.ai.word.model.Wordbook;
 
@@ -24,6 +26,7 @@ public class WordbookResult {
 		ApiResult apiResult = ApiResult.blank()
 				.add("id", wb.getId().toString())
 				.add("user_id", wb.getUser_id())
+				.add("name", wb.getName())
 				.add("words", wordsData)
 				.add("time", wb.getId());
 		return Result.ok(apiResult);
@@ -31,8 +34,22 @@ public class WordbookResult {
 	
 	
 	public static ResponseEntity<ApiResult> build(List<Wordbook> wb_list) {
+		ArrayList<WordbookData> wordbooksData = new ArrayList<>();
+		for(Wordbook wb : wb_list) {
+			List<WordData> wordsData = new ArrayList<>();
+			for(Word word : wb.getWords()) {
+				wordsData.add(new WordData(word.getId().toString(),
+											word.getEng(),
+											word.getKor(),
+											word.isCheck()));
+			}			
+			wordbooksData.add(new WordbookData(wb.getId().toString(),
+											wb.getName(),
+											wb.getUser_id(),
+											wordsData));
+		}
 		ApiResult apiResult = ApiResult.blank()
-				.add("wordbooks", wb_list);
+				.add("wordbooks", wordbooksData);
 		return Result.ok(apiResult);
 	}
 	
@@ -43,5 +60,14 @@ public class WordbookResult {
 		private String eng;
 		private String kor;
 		private boolean check;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	private static class WordbookData {
+		private String id;
+		private String name;
+		private String user_id;
+		private List<WordData> words;
 	}
 }
