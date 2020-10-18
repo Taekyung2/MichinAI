b2dfcd5849da85b9450b634dee392d5bec0afea9
김송희 <girawhale@naver.com>
2020-10-18 07:07:27 +0000

    Delete Django

--- backend_dj/word/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'word'
-
-urlpatterns = [
-    path('list/<int:userid>/', views.wordbook_list),
-    path('<int:wordbookid>/', views.word_list),
-]
